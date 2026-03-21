dc8d88c785a905c352537736c95604fafa0790e8
pauloflau <pauloflau@gmail.com>
2026-03-21 19:01:13 -0300

    feat(service): implementar lógica no AlunoService

--- backend/src/main/java/com/jmp/academia/services/AlunoService.java
+++ backend/src/main/java/com/jmp/academia/services/AlunoService.java
@@ -1,6 +1,7 @@
 package com.jmp.academia.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jmp.academia.dtos.AlunoDto;
 import com.jmp.academia.entity.Aluno;
@@ -13,11 +14,34 @@ import lombok.RequiredArgsConstructor;
 public class AlunoService {
 	private final AlunoRepository alunoRepository;
 	
-	public void insert(AlunoDto dto) {
+	@Transactional
+	public AlunoDto insert(AlunoDto dto) {
 		Aluno aluno = new Aluno();
 		aluno.setMatricula(dto.getMatricula());
-		//restante dos metodos
-	}
-	
-	
+		aluno.setSexo(dto.getSexo());
+		aluno.setRg(dto.getRg());
+		aluno.setDataNascimento(dto.getDataNascimento());
+		aluno.setSituacao(dto.getSituacao());
+		aluno.setEmail(dto.getEmail());
+		
+		aluno = alunoRepository.save(aluno);
+		
+		dto.setMatricula(aluno.getMatricula());
+		dto.setSexo(aluno.getSexo());
+		dto.setRg(aluno.getRg());
+		dto.setDataNascimento(aluno.getDataNascimento());
+		dto.setSituacao(aluno.getSituacao());
+		dto.setEmail(aluno.getEmail());
+		
+		return dto;
+	}	
 }
+/*
+ * 
+	@Embedded //O ENDERECO FAZ PARTE DE ALUNO E COMPARTILHA O ID DE ALUNO
+	private Endereco endereco = new Endereco();
+	
+	@Embedded //O TELEFONE FAZ PARTE DE ALUNO E COMPARTILHA O ID DE ALUNO
+	private Telefone telefone = new Telefone();
+ * */
+ 
