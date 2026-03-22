3e7ed004cb73ca841a21a2f0870b11b1fb6b8579
pauloflau <pauloflau@gmail.com>
2026-03-21 21:07:50 -0300

    fix(AlunoDto): refazer construtor de entidade

--- backend/src/main/java/com/jmp/academia/dtos/AlunoDto.java
+++ backend/src/main/java/com/jmp/academia/dtos/AlunoDto.java
@@ -26,8 +26,8 @@ public class AlunoDto {
 	private LocalDate dataNascimento;
 	private Situacao situacao;
 	private String email;
-	private EnderecoDto endereco = new EnderecoDto();
-	private TelefoneDto telefone = new TelefoneDto();
+	private EnderecoDto enderecoDto = new EnderecoDto();
+	private TelefoneDto telefoneDto = new TelefoneDto();
 	
 	public AlunoDto(Aluno entidade) {
 		this.matricula = entidade.getMatricula();
@@ -36,6 +36,20 @@ public class AlunoDto {
 		this.rg = entidade.getRg();
 		this.dataNascimento = entidade.getDataNascimento();
 		this.situacao = entidade.getSituacao();
-		this.email = entidade.getEmail();		
+		this.email = entidade.getEmail();	
+		
+		this.enderecoDto.setRua(entidade.getEndereco().getRua());
+		this.enderecoDto.setNumero(entidade.getEndereco().getNumero()); 
+		this.enderecoDto.setComplemento(entidade.getEndereco().getComplemento());
+		this.enderecoDto.setCidade(entidade.getEndereco().getCidade());
+		this.enderecoDto.setCep(entidade.getEndereco().getCep());
+		this.enderecoDto.getEstado().setNome(entidade.getEndereco().getEstado().getNome());
+		this.enderecoDto.getEstado().setSigla(entidade.getEndereco().getEstado().getSigla());
+		
+		this.telefoneDto.setDddCelular(entidade.getTelefone().getDddCelular());
+		this.telefoneDto.setNumeroCelular(entidade.getTelefone().getNumeroCelular());	
+		this.telefoneDto.setDddFixo(entidade.getTelefone().getDddFixo());
+		this.telefoneDto.setNumeroFixo(entidade.getTelefone().getNumeroFixo());	
+
 	}
 }
